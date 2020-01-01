36641d51ff45bb280cda79911176f1d9a567c0f7
Jelena Telenius <jelena.telenius@gmail.com>
2020-01-01 22:45:53 +0000

    adding license

--- bashHelpers/fileTesters.sh
+++ bashHelpers/fileTesters.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+##########################################################################
+# Copyright 2017, Jelena Telenius (jelena.telenius@imm.ox.ac.uk)         #
+#                                                                        #
+# This file is part of GEObuilder .                                      #
+#                                                                        #
+# GEObuilder is free software: you can redistribute it and/or modify     #
+# it under the terms of the MIT license.
+#
+#
+#                                                                        #
+# GEObuilder is distributed in the hope that it will be useful,          #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of         #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          #
+# MIT license for more details.
+#                                                                        #
+# You should have received a copy of the MIT license
+# along with GEObuilder.  
+##########################################################################
+
 doTempFileTesting(){
     
     # NEEDS THIS TO BE SET BEFORE CALL :
